343fc62c257eb8914a6657b9dd40fc11655e8985
Erika Moate <erikamoatemail@gmail.com>
2016-11-05 11:09:31 -0700

    Create processConfig function.

--- moate_erika_assignment1.cpp
+++ moate_erika_assignment1.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -38,7 +39,23 @@ void handleError(String message) {
 
 /*
  * Processes the configuration file for text highlighting.
+ *
+ * Reads in line by line. If line has at least two words, adds them to the map.
+ * If the key exits in the map, nothing is inserted.
+ *
+ * Example input line:
+ * bold  \e[0;31m  #red
  */
-bool processConfig(map<String, String> &highlight) {
-	
+bool processConfig(const map<String, String> &highlight
+				   ifstream &is) {
+	String line, key, color;
+	stringstream ss;
+
+	while (getline(is, line)) {
+		ss.str(line);
+		if (ss >> key) {
+			if (ss >> color) {
+				highlight.insert(pair<String, String>(key, color));
+			}
+	}
 }
